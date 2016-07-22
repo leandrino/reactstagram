c3c8664510d50b693f9d9227b17e260028c071b4
Leandro Alexandrino <hello@meche.space>
2016-07-21 22:50:00 -0300

    stopped in class 16

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -24,13 +24,14 @@ const Photo = React.createClass({
 						<p>{post.caption}</p>
 						<div className='control-buttons'>
 							<button className='likes' onClick={this.props.increment.bind(null, i)}>&hearts; {post.likes}</button>
+						
+							<Link className='button' to={`/view/${post.code}`}>
+								<span className='comment-count'>
+									<span className='speech-bubble'> </span>
+									{comments[post.code] ? comments[post.code].length : 0}
+								</span>
+							</Link>
 						</div>
-						<Link className='button' to={`/view/${post.code}`}>
-							<span className='comment-count'>
-								<span className='speech-bubble'></span>
-								{comments[post.code] ? comments[post.code].length : 0}
-							</span>
-						</Link>
 					</figcaption>	
 
 				</div>
